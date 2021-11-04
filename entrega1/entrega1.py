02143b47b354a6af94d0549c02ac5db2c202d83b
Juan Carignano <69175258+juancarignano@users.noreply.github.com>
2021-11-04 14:52:05 -0300

    Update

--- entrega1/entrega1.py
+++ entrega1/entrega1.py
@@ -1,206 +1,143 @@
-from simpleai.search import (
-    SearchProblem,
-    astar,
-)
+from simpleai.search import (SearchProblem,astar)
+from simpleai.search.viewers import BaseViewer
 
-# Tenemos que saber: 
-# OK- donde esta el robot 
-# - cual es el camino que ya recorrio (conocer cuales casilleros ya fueron recorridos)
-# - que tipo de robot es (mapeador - soporte)
+    # Tenemos que saber: 
+    # donde esta el robot 
+    # - cual es el camino que ya recorrio (conocer cuales casilleros ya fueron recorridos)
+    # - que tipo de robot es (mapeador - soporte)
 
-# - escaneador -> puede moverse un máximo de 10 movimientos (1000 mAh / 100 mah por movimiento)
-# - soporte -> mismo consumo de movientos (costo), que el mapeador
-# soporte carga en 5 min al mapeador (por ende, debemos tener guardada la posicion que logro recorrer el mapeador)
-# llevar constancia del nivel de bateria del mapeador (ya que no necesariamente debe ser cargado al agotar su bateria)
-# la bateria del mapeador se carga en su totalidad
+    # - escaneador -> puede moverse un máximo de 10 movimientos (1000 mAh / 100 mah por movimiento)
+    # - soporte -> mismo consumo de movientos (costo), que el mapeador
+    # soporte carga en 5 min al mapeador (por ende, debemos tener guardada la posicion que logro recorrer el mapeador)
+    # llevar constancia del nivel de bateria del mapeador (ya que no necesariamente debe ser cargado al agotar su bateria)
+    # la bateria del mapeador se carga en su totalidad
 
-# 1 robot a la vez (turnos)
+def search_robot(robots,id):
+    return list(filter(lambda robot: robot[1][0] == id, enumerate(robots)))
 
-# Método para convertir tuplas a listas  
-def convertir_a_listas(tuplas):
+# Convertidor de tuplas a listas  
+def to_list(tuplas):
     return [list(x) for x in tuplas]
 
-# Método para convertir listas a tuplas
-def convertir_a_tuplas(listas):
+# Convertidor de listas a tuplas
+def to_tuple(listas):
     return tuple([tuple(x) for x in listas])
 
 def planear_escaneo(tuneles, robots):
+    #nuestro estado inicial sera una tupla donde la primer parte de la tupla son los tuneles, y en la segunda, una tupla con el listado de robots  
+    initial_state = (tuneles,[]) #le agregamos la lista de tuplas con las posiciones de los casilleros
 
-    initial_tunnels = []
-    initial_robots = []
+    for robot in robots: #recorremos la lista de robots
+        if(robot[1] == "escaneador"):
+            initial_state[1].append((robot[0],"escaneador", (5,0), 1000)) #id robot, posicion, bateria, tipo
+        else:
+            initial_state[1].append((robot[0],"soporte", (5,0), 10000))
+            
+    initial_state = to_tuple(initial_state)
 
-    # Recorremos los túneles y los agregamos al initial_state.
-    # False que pasamos en la tupla = casillero no recorrido.
-    for box in tuneles:
-        initial_tunnels.append((box, False))
-    
-    # Recorremos los robots y los agregamos al initial_state (robot, posición, batería).
-    for robot in robots:
-        id_robot, robot_type = robot
-        battery = 10 # 10 de bateria = 100% (llena)
-        initial_robots.append((id_robot, robot_type, (5, 0), battery))
 
-    initial_state = (tuple(initial_tunnels), tuple(initial_robots))
-
-    """
-    Determinar si una casilla existen en el recorrido planteado para la mina.
-    """
-    def exists(position, boxs):
-        for boxes in boxs:
-            if position == boxes[0]:
-                return True
-        return False
-
+    # Clase del problema
     class RobotsMineriaProblem(SearchProblem):
 
+        # El costo sera los minutos a moverse o el tiempo en cargar
         def cost(self, state1, action, state2):
-            if action[1] == "Mover":
+            id_robot, action_type, new_position_id_robot_cargar = action
+            if(action_type == 'mover'):
                 return 1
-            else: 
-                return 5 # costo de cargar = 5
-
-        def is_goal(self, state):
-            boxs, robots = state
-
-            for box in boxs:
-                if box[1] == False:
-                    return False
-            return True # todas las casillas con box[1] = True, quiere decir que recorrió todas las casillas.
-
-
-        """
-        mapeador -> moverse
-        soporte -> cargar 
-        accion = robot -> (
-            id robot que hace la accion,
-            tipo de acción,
-            nueva posición del robot/id robot a cargar
-        )
-        """
-
+            return 5
+        
+        # Las acciones son tuplas de 3 elementos: (id_robot_realiza_accion, tipo_accion, nueva_posicion o id_robot_cargar)
         def actions(self, state):
             available_actions = []
-            boxs, robots = state
-            
+
+            # separamos tuneles de los robots del estado
+            tuneles_pendientes, robots = state
+
             for actual_robot in robots:
 
-                id_robot, robot_type, robot_position, battery = actual_robot # id, tipo de robot, posición y batería.
-                row_robot, col_robot = robot_position #fila, columna
+                # Desglosar la información de los robots
+                id_robot, type, position, batery = actual_robot
+                
+                if  batery >=100: # vemos las distintas posiciones donde se movera no importa el tipo ya que ambos se deben mover
+                    posible_move = [] #Lista de posible movimientos
+                    posible_move.append((position[0]-1, position[1])) # movimiento hacia abajo
+                    posible_move.append((position[0]+1, position[1])) #movimienito hacia arriba
+                    posible_move.append((position[0], position[1]-1)) #movimiento hacia izquierda
+                    posible_move.append((position[0], position[1]+1)) #movimiento hacia derecha
+                    # de aca, algunas no son posibles acceder, por ende hay que filtrarlas:
+                    for new_position in posible_move:
+                        if new_position in tuneles:
+                            available_actions.append((id_robot, 'mover', new_position))
 
-                if robot_type ==  "escaneador" and battery > 0:
-                        #filas de la nro 1 hasta la 11 ya que la 0 no tiene otra atras 
-                        if row_robot > 0:
-                            new_position = (row_robot - 1, col_robot)
-                            if exists(new_position, boxs):
-                                available_actions.append((id_robot, "Mover", new_position))         
-                        #filas de la nro 0 hasta la 10 ya que la 11 no tiene otra siguiente
-                        if row_robot < 11:
-                            new_position = (row_robot + 1, col_robot)
-                            if exists(new_position, boxs):
-                                available_actions.append((id_robot, "Mover", new_position))
-                        #columna de la nro 1 hasta la 11 ya que la 0 no tiene una a la izq.
-                        if col_robot > 0:
-                            new_position = (row_robot, col_robot - 1)
-                            if exists(new_position, boxs):
-                                available_actions.append((id_robot, "Mover", new_position))  
-                        #columna de la nro 0 hasta la 10 ya que la 11 no tiene una a la izq.
-                        if col_robot < 11:
-                            new_position = (row_robot, col_robot + 1)
-                            if exists(new_position, boxs):
-                                available_actions.append((id_robot, "Mover", new_position))
+                # generar acciones de carga para los robots de carga
+                if type == "soporte":
+                    for robot_to_charge in robots:
+                        if robot_to_charge[2] == position and robot_to_charge[3] < 1000 and robot_to_charge[3] >= 0:
+                            available_actions.append((id_robot, 'cargar', robot_to_charge[0]))
 
-                else: # robot soporte
-                    for robot in robots:
-                        if robot[1] == "escaneador" and robot[3] < 10 and robot[2] == robot_position :  #si el robot soporte esta en la misma posicion que un escaneador que no tiene bateria full, da la opcion de recargarlo
-                            available_actions.append((id_robot, "Cargar", robot[0])) 
-                    
-                    #ahora le pasamos los distintos lugares donde se puede mover el soporte.
-                    #filas de la nro 1 hasta la 11 ya que la 0 no tiene otra atras
-                    if row_robot > 0:
-                        new_position = (row_robot - 1, col_robot) #vemos si el casillero de abajo esta disponible
-                        if exists(new_position, boxs):
-                            available_actions.append((id_robot, "Mover", new_position))         
-                    #filas de la nro 0 hasta la 10 ya que la 11 no tiene otra siguiente
-                    if row_robot < 11:
-                        new_position = (row_robot + 1, col_robot)
-                        if exists(new_position, boxs):
-                            available_actions.append((id_robot, "Mover", new_position))
-                    #columna de la nro 1 hasta la 11 ya que la 0 no tiene una a la izq.
-                    if col_robot > 0:
-                        new_position = (row_robot, col_robot - 1)
-                        if exists(new_position, boxs):
-                            available_actions.append((id_robot, "Mover", new_position))  
-                    #columna de la nro 0 hasta la 10 ya que la 11 no tiene una a la izq.
-                    if col_robot < 11:
-                        new_position = (row_robot, col_robot + 1)
-                        if exists(new_position, boxs):
-                            available_actions.append((id_robot, "Mover", new_position))
-            
-            
             return available_actions
 
         def result(self, state, action):
-            state_as_lists = list(list(row) for row in state)
-            boxs, robots = state_as_lists
+            # separar estado
+            pending_tunnels, robots = state
+            # Pasamos a lista para poder modificar
+            pending_tunnels = list(pending_tunnels)
+            
+            # Convertir a listas las tuplas de estado robots para luego poder modificarlas
+            robots = to_list(robots)
+            
+            # separamos las distintas partes del action
+            id_robot, action_type, new_position_id_robot_carga = action
 
-            boxs_as_lists = list(list(row) for row in boxs)
-            robots_as_lists = list(list(row) for row in robots)
+            # Si el tipo de acción es mover, 
+            # modificar el estado asignándole al robot correspondiente la nueva posición
+            if(action_type == 'mover'):
+                # buscamos en la lista de robots el robot que hace la accion
+                robot_to_move = search_robot(robots,id_robot)
+                robot = robot_to_move[0] 
+                id_robot_move = robot[0]
+                robots[id_robot_move][2] = new_position_id_robot_carga
 
-            # 1- ver que tipo de accion es
-            if action[1] == "Mover":
-                #asigno al robot la nueva ubicacion 
-                for robot in robots_as_lists:
-                    if robot[0] == action[0]: #encontramos al robot que indica la accion
-                        robot[2] = action[2]  #el robot en la pos 2 tiene la ubicacion actual, accion en 2 tiene la ubicacion nueva
-                        # revisamos si el tipo 
-                        if robot[1] == "escaneador":
-                            #restamos bateria
-                            robot[3] -= 1
-                            #indico que la nueva casilla a la que se movio el robot pasa a ser escaneada
-                            for box in boxs_as_lists:   # recorremos las casillas 
-                                if box[0] == action[2]: # encontramos la posición nueva
-                                    box[1] = True       # asignamos true al [1] de box --> ya está escaneada
+                # Si es escaneador (no de soporte) restar 100 a la batería
+                if(robot[1][1] == "soporte"):
+                    robots[id_robot_move][3] -= 100
+                    # Eliminar la coordenada del túnel que se está escaneando de 
+                    # la lista de túneles pendientes
+                    if new_position_id_robot_carga in pending_tunnels:
+                                pending_tunnels.remove(new_position_id_robot_carga)
+            else:
+                # Si el tipo de acción es cargar, modificar la batería del robot que se quiere cargar a 1000 new_position_id_robot_carga
+                robot_to_charge = search_robot(robots,new_position_id_robot_carga)
+                robot_charge = robot_to_charge[0] 
+                id_robot_charged = robot_charge[0]
+                robots[id_robot_charged][3] = 1000
 
+            # Convertir a tuplas los elementos del estado
+            pending_tunnels = tuple(pending_tunnels)
+            robots = to_tuple(robots)
 
-            else: #es cargar
-                for robot in robots_as_lists:
-                    if robot[0] == action[0]: # encontramos el robot soporte que va a cargar
-                        for scanners in robots_as_lists:
-                            if scanners[0] == action[2]: #encontramos el robot al que se le va cargar la bateria
-                                scanners[3] = 10       
-    
-            #pasamos a tupla para que quede como el estado.
-            # Casilleros (posicion, recorrido true/false)
-            # Robots (id, tipo, posicion, cant bateria)
-            new_boxs = tuple(tuple(row) for row in boxs_as_lists)
-            new_robots = tuple(tuple(row) for row in robots_as_lists)
-
-            new_state = (new_boxs, new_robots)
+            return (pending_tunnels, robots)
         
-            return new_state
-
-
-        """
-        Cantidad de casillas no visitas.
-        """
-        def heuristic(self, state):
+        def is_goal(self, state):
+            #separamos estado de robots
             boxs, robots = state
-            boxs_not_visited = 0
-
-            for box in boxs:
-                if box[1] == False:
-                    boxs_not_visited += 1
-            return boxs_not_visited
+            #si quedan en el estado casillas por recorrrer devolvera false, sino true.
+            return len(boxs) == 0
         
-    problem = RobotsMineriaProblem(initial_state)
+        def heuristic(self, state):
+            # La heurística es la cantidad de túneles que faltan recorrer
+            # multiplicados por 1 minuto
+            return (len(state[0]))
+                    
+    problema = RobotsMineriaProblem(initial_state)
 
-    result = astar(problem, graph_search=True)
+    resultado = astar(problema, graph_search=True)
 
     plan = []
 
     # Recorrer el resultado agregando a la lista plan, 
     # las acciones seleccionadas por el algoritmo
-    for action, state in result.path():
+    for action, state in resultado.path():
         # Descartar la primera acción que es None
         if (action is not None):
             plan.append(action)
