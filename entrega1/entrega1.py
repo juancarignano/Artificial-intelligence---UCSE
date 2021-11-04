0a02f5212c8d7580b2f1a563a5aa392f2afc4025
Juan Carignano <69175258+juancarignano@users.noreply.github.com>
2021-11-03 22:54:08 -0300

    Correciones entrega1

--- entrega1/entrega1.py
+++ entrega1/entrega1.py
@@ -16,7 +16,7 @@ from simpleai.search.viewers import WebViewer, BaseViewer
 # - cual es el camino que ya recorrio (conocer cuales casilleros ya fueron recorridos)
 # - que tipo de robot es (mapeador - soporte)
 
-# - mapeador -> puede moverse un máximo de 10 movimientos (1000 mAh / 100 mah por movimiento)
+# - escaneador -> puede moverse un máximo de 10 movimientos (1000 mAh / 100 mah por movimiento)
 # - soporte -> mismo consumo de movientos (costo), que el mapeador
 # soporte carga en 5 min al mapeador (por ende, debemos tener guardada la posicion que logro recorrer el mapeador)
 # llevar constancia del nivel de bateria del mapeador (ya que no necesariamente debe ser cargado al agotar su bateria)
@@ -28,8 +28,10 @@ from simpleai.search.viewers import WebViewer, BaseViewer
 Determinar si una casilla existen en el recorrido planteado para la mina.
 """
 def exists(position, boxs):
-    if position in boxs[0]:
-        return True
+    for boxes in boxs:
+        if position == boxes[0]:
+            return True
+    return False
 
 class RobotsMineriaProblem(SearchProblem):
     def __init__(self, tunnels, robots):
@@ -70,11 +72,10 @@ class RobotsMineriaProblem(SearchProblem):
     """
     mapeador -> moverse
     soporte -> cargar 
-
     accion = robot -> (
         id robot que hace la accion,
         tipo de acción,
-        nueva posición del robot
+        nueva posición del robot/id robot a cargar
     )
     """
 
@@ -84,36 +85,59 @@ class RobotsMineriaProblem(SearchProblem):
         
         for actual_robot in robots:
 
-            id_robot, robot_type, robot_position, battery = actual_robot # tipo de robot, posición y batería.
-            row_robot, col_robot = robot_position 
+            id_robot, robot_type, robot_position, battery = actual_robot # id, tipo de robot, posición y batería.
+            row_robot, col_robot = robot_position #fila, columna
 
             if robot_type ==  "escaneador" and battery > 0:
-                    # the piece above
+                    #filas de la nro 1 hasta la 11 ya que la 0 no tiene otra atras 
                     if row_robot > 0:
                         new_position = (row_robot - 1, col_robot)
                         if exists(new_position, boxs):
                             available_actions.append((id_robot, "Mover", new_position))         
-                    # the piece below
-                    if row_robot < 9:
+                    #filas de la nro 0 hasta la 10 ya que la 11 no tiene otra siguiente
+                    if row_robot < 11:
                         new_position = (row_robot + 1, col_robot)
                         if exists(new_position, boxs):
                             available_actions.append((id_robot, "Mover", new_position))
-                    # the piece to the left
+                    #columna de la nro 1 hasta la 11 ya que la 0 no tiene una a la izq.
                     if col_robot > 0:
                         new_position = (row_robot, col_robot - 1)
                         if exists(new_position, boxs):
                             available_actions.append((id_robot, "Mover", new_position))  
-                    # the piece to the right
-                    if col_robot < 9:
+                    #columna de la nro 0 hasta la 10 ya que la 11 no tiene una a la izq.
+                    if col_robot < 11:
                         new_position = (row_robot, col_robot + 1)
                         if exists(new_position, boxs):
                             available_actions.append((id_robot, "Mover", new_position))
 
             else: # robot soporte
                 for robot in robots:
-                    if robot[1] == "escaneador" and robot[3] < 10:
-                        available_actions.append(id_robot, "Cargar", robot[2]) # la posición es robot[2] ya que es la actual del robot al cual se va a cargar.
-
+                    if robot[1] == "escaneador" and robot[3] < 10 and robot[2] == robot_position :  #si el robot soporte esta en la misma posicion que un escaneador que no tiene bateria full, da la opcion de recargarlo
+                        available_actions.append((id_robot, "Cargar", robot[0])) 
+                
+                #ahora le pasamos los distintos lugares donde se puede mover el soporte.
+                #filas de la nro 1 hasta la 11 ya que la 0 no tiene otra atras
+                if row_robot > 0:
+                    new_position = (row_robot - 1, col_robot) #vemos si el casillero de abajo esta disponible
+                    if exists(new_position, boxs):
+                        available_actions.append((id_robot, "Mover", new_position))         
+                #filas de la nro 0 hasta la 10 ya que la 11 no tiene otra siguiente
+                if row_robot < 11:
+                    new_position = (row_robot + 1, col_robot)
+                    if exists(new_position, boxs):
+                        available_actions.append((id_robot, "Mover", new_position))
+                #columna de la nro 1 hasta la 11 ya que la 0 no tiene una a la izq.
+                if col_robot > 0:
+                    new_position = (row_robot, col_robot - 1)
+                    if exists(new_position, boxs):
+                        available_actions.append((id_robot, "Mover", new_position))  
+                #columna de la nro 0 hasta la 10 ya que la 11 no tiene una a la izq.
+                if col_robot < 11:
+                    new_position = (row_robot, col_robot + 1)
+                    if exists(new_position, boxs):
+                        available_actions.append((id_robot, "Mover", new_position))
+        
+        
         return available_actions
 
     def result(self, state, action):
@@ -123,19 +147,32 @@ class RobotsMineriaProblem(SearchProblem):
         boxs_as_lists = list(list(row) for row in boxs)
         robots_as_lists = list(list(row) for row in robots)
 
-        # 1- actualizar posición del robot que esta ejecutando la acción (OK)
-        # 2- decirle a la nueva casilla que ya esta escaneada
-
-        for robot in robots_as_lists:
-            if robot[0] == action[0]:
-                robot[2] = action[2] # asigno al robot que hace la acción, la posición nueva (generada en el acción).
-            
-
+        # 1- ver que tipo de accion es
         if action[1] == "Mover":
-            for box in boxs_as_lists:            # recorremos las casillas 
-                if box[0] == action[2]: # encontramos la posición nueva
-                    box[1] = True       # asignamos true al [1] de box --> ya está escaneada
+            #asigno al robot la nueva ubicacion 
+            for robot in robots_as_lists:
+                if robot[0] == action[0]: #encontramos al robot que indica la accion
+                    robot[2] = action[2]  #el robot en la pos 2 tiene la ubicacion actual, accion en 2 tiene la ubicacion nueva
+                     # revisamos si el tipo 
+                    if robot[1] == "escaneador":
+                        #restamos bateria
+                        robot[3] -= 10
+                        #indico que la nueva casilla a la que se movio el robot pasa a ser escaneada
+                        for box in boxs_as_lists:   # recorremos las casillas 
+                            if box[0] == action[2]: # encontramos la posición nueva
+                                box[1] = True       # asignamos true al [1] de box --> ya está escaneada
 
+
+        else: #es cargar
+            for robot in robots_as_lists:
+                if robot[0] == action[0]: # encontramos el robot soporte que va a cargar
+                    for scanners in robots_as_lists:
+                        if scanners[0] == action[2]: #encontramos el robot al que se le va cargar la bateria
+                            scanners[3] = 10       
+ 
+        #pasamos a tupla para que quede como el estado.
+        # Casilleros (posicion, recorrido true/false)
+        # Robots (id, tipo, posicion, cant bateria)
         new_boxs = tuple(tuple(row) for row in boxs_as_lists)
         new_robots = tuple(tuple(row) for row in robots_as_lists)
 
